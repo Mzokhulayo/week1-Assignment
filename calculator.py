807f157a6af3383a862211524717496577e062db
Mzokhulayo <nkululeko.mdubeki@hotmail.com>
2025-03-11 15:56:51 +0200

    changed logic to give correct output

--- calculator.py
+++ calculator.py
@@ -13,22 +13,23 @@ operator = input("Which operator to apply?\n"
 
 def Calculater_logic(num1,num2,opr):
     if operator == "+":
-        return number_1 + number_2
+        return f"{number_1} + {number_2} = {number_1+number_2}"
         
     elif operator == "-":
-        return number_1 - number_2
+        return f"{number_1} - {number_2} = {number_1-number_2}"
     
     elif operator == "*":
-        return number_1 * number_2
+        return f"{number_1} * {number_2} = {number_1*number_2}"
         
     elif operator == "/":
         if number_2 == 0:
             return "Error: Division by zero is not allowed."
-        return number_1 / number_2
+        return f"{number_1} / {number_2} = {number_1/number_2}"
     else:
         return "Invalid operator\n" \
         "insert valid operator [+-/*] only"
+    
         
     
 solution = Calculater_logic(number_1, number_2, operator)
-print(f"Your Answer is: {solution}")
+print(solution)
